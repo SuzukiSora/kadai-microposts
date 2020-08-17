298a3891f64b31e1c62eee2f95ee2856178c2c34
sora.suzuki <sorasuzuki1214@gmail.com>
2020-08-17 12:02:03 +0000

    likes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,13 +7,13 @@ class FavoritesController < ApplicationController
     favorite = Micropost.find(params[:favorite_id])
     current_user.favorite(favorite)
     flash[:success] = 'お気に入りに登録しまいした。'
-    redirect_to root_path
+    redirect_to user_path(current_user)
   end
 
   def destroy
     favorite = Micropost.find(params[:favorite_id])
     current_user.unfavorite(favorite)
     flash[:success] = 'お気に入りから削除しました。'
-    redirect_to root_path
+    redirect_to user_path(current_user)
   end
 end
